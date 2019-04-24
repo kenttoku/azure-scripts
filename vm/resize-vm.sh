7a13dc6f3200b60acd24251b4efdc48175af6d28
Kent Tokunaga <KentTokunaga@gmail.com>
2019-04-24 17:33:24 -0500

    Add validations for resize-vm

---
+++ vm/resize-vm.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+## Script to resize a vm
+## Variables
+vm_name=$1
+resource_group=$2
+
+## Validate Variables
+if [ -z "$vm_name" ]; then
+  echo "You must provide a name for your VM." 1>&2
+  exit 1
+fi
+
+if [ -z "$resource_group" ]; then
+  echo "You must provide a resource group." 1>&2
+  exit 1
+fi
+
+## Check for resource group.
+echo "Validating resource group."
+if [ "$(az group exists --name $resource_group)" = "false" ]; then
+  echo "Resource group does not exist." 1>&2
+  exit 1
+fi
+
+## Checking existing VM for duplicates
+echo "Validating VM name."
+if [ -z "$(az vm list -g $resource_group --query [].name | grep "\"$vm_name\"")" ]; then
+  echo "A VM with the name $vm_name does not exist." 1>&2
+  exit 1
+fi
+
+## Script to delete a vm
