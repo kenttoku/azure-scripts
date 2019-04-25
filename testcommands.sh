608f175bcdef6dcfee6a8b2bd7e7186ec2e2d5e4
Kent Tokunaga <KentTokunaga@gmail.com>
2019-04-25 13:54:51 -0500

    Add brew to path between scripts

--- testcommands.sh
+++ testcommands.sh
@@ -6,6 +6,9 @@ case "$command" in
   "createvm")
     vm/vm.sh create testvm vm-group southcentralus UbuntuLTS Standard_B1s kent
     ;;
+  "deletevm")
+    vm/vm.sh delete testvm vm-group
+    ;;
   *)
     echo "Invalid command" 1>&2
     exit 1
--- vm/server.sh
+++ vm/server.sh
@@ -7,6 +7,11 @@ user_name=$3
 ## Setup linux environment
 curl https://raw.githubusercontent.com/kenttoku/revature-p0/master/linux-setup.sh | bash
 
+test -d ~/.linuxbrew && eval "$(~/.linuxbrew/bin/brew shellenv)"
+test -d /home/linuxbrew/.linuxbrew && eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+test -r ~/.bash_profile && echo "eval \$($(brew --prefix)/bin/brew shellenv)" >>~/.bash_profile
+echo "eval \$($(brew --prefix)/bin/brew shellenv)" >>~/.profile
+
 ## Setup git project structure
 curl https://raw.githubusercontent.com/kenttoku/revature-p0/master/git-node-project.sh | bash -s "$directory" "$user_email" "$user_name"
 
