8fc358dd3dda0bcc8691dbcae9fd6ddc84484dca
Kent Tokunaga <KentTokunaga@gmail.com>
2019-04-25 12:34:35 -0500

    Set up and run server.js

--- vm/server.sh
+++ vm/server.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
-# directory=$1
-# user_email=$2
-# user_name=$3
+directory=$1
+user_email=$2
+user_name=$3
 
-# ## Setup linux environment
-# curl https://raw.githubusercontent.com/kenttoku/revature-p0/master/linux-setup.sh | bash
+## Setup linux environment
+curl https://raw.githubusercontent.com/kenttoku/revature-p0/master/linux-setup.sh | bash
 
-# ## Setup git project structure
-# curl https://raw.githubusercontent.com/kenttoku/revature-p0/master/git-node-project.sh | bash -s "$directory" "$user_email" "$user_name"
+## Setup git project structure
+curl https://raw.githubusercontent.com/kenttoku/revature-p0/master/git-node-project.sh | bash -s "$directory" "$user_email" "$user_name"
 
-# ## Setup Server
-# cd "$directory"
+## Setup Server
+cd "$directory" || exit 1
 curl https://raw.githubusercontent.com/kenttoku/azure-scripts/master/vm/server.js > server.js
+node server.js
