a91937b02792d1f42316d911df95410aba8f006c
Kent Tokunaga <KentTokunaga@gmail.com>
2019-04-25 09:11:21 -0500

    Create main function

--- vm/disk.sh
+++ vm/disk.sh
@@ -23,26 +23,30 @@ delete () {
 }
 
 ## Main
-command=$1
-disk_name=$2
-resource_group=$3
+main () {
+  command=$1
+  disk_name=$2
+  resource_group=$3
 
-## Check for azure-cli
-if [ -z "$(which az)" ]; then
-  echo "No azure-cli. Please install before continuing." 1>&2
-  exit 1
-fi
-
-## Validate required arguments
-validate_arg "$command" "command"
-validate_arg "$disk_name" "vm_name"
-validate_arg "$resource_group" "resource_group"
-
-case "$command" in
-  "create") create;;
-  "delete") delete;;
-  *)
-    echo "Invalid command." 1>&2
+  ## Check for azure-cli
+  if [ -z "$(which az)" ]; then
+    echo "No azure-cli. Please install before continuing." 1>&2
     exit 1
-    ;;
-esac
+  fi
+
+  ## Validate required arguments
+  validate_arg "$command" "command"
+  validate_arg "$disk_name" "vm_name"
+  validate_arg "$resource_group" "resource_group"
+
+  case "$command" in
+    "create") create;;
+    "delete") delete;;
+    *)
+      echo "Invalid command." 1>&2
+      exit 1
+      ;;
+  esac
+}
+
+main "$@"
