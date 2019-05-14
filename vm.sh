33fa48e7ce4b7dbe11654c90502131c005f5479d
Kent Tokunaga <KentTokunaga@gmail.com>
2019-05-14 14:02:07 -0500

    Fix vm script

--- vm.sh
+++ vm.sh
@@ -2,7 +2,9 @@
 command=$1
 
 case "$command" in
-  "create") virutal-machine/create-vm.sh;;
+  "create") virutal-machine/create-vm.sh "${@:2}";;
+  "delete") virutal-machine/delete-vm.sh "${@:2}";;
+  "list") virutal-machine/list-vm.sh "${@:2}";;
   *)
     echo "Invalid command." 1>&2
     exit 1
