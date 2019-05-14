f395ee1684869f699bcb4e32e594303803aaa098
Kent Tokunaga <KentTokunaga@gmail.com>
2019-05-14 14:00:46 -0500

    Delete VM

--- virtual-machine/delete-vm.sh
+++ virtual-machine/delete-vm.sh
@@ -1,1 +1,13 @@
 #!/bin/bash
+
+# Variables
+vm_name=$1
+resource_group=$2
+
+# Delete VM
+echo "Deleting VM."
+az vm delete \
+  --resource-group "$resource_group" \
+  --name "$vm_name" \
+  --yes
+echo "VM deleted."
