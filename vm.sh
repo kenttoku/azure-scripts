95b215601fe6253c2fea34945c2b62d00c98e7ea
Kent Tokunaga <KentTokunaga@gmail.com>
2019-05-14 14:18:41 -0500

    Fix typo

--- vm.sh
+++ vm.sh
@@ -2,9 +2,9 @@
 command=$1
 
 case "$command" in
-  "create") virutal-machine/create-vm.sh "${@:2}";;
-  "delete") virutal-machine/delete-vm.sh "${@:2}";;
-  "list") virutal-machine/list-vm.sh "${@:2}";;
+  "create") virtual-machine/create.sh "${@:2}";;
+  "delete") virtual-machine/delete.sh "${@:2}";;
+  "list") ./virtual-machine/list.sh "${@:2}";;
   *)
     echo "Invalid command." 1>&2
     exit 1
